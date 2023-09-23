eefee74c4e56fb6a1ff0910a08cb5dadf630b1ed
Gustavo Ian <gustavoian1790@gmail.com>
2023-09-23 13:17:12 -0300

    feat: inicio do retorno de dados

--- static/info-campus.php
+++ static/info-campus.php
@@ -26,6 +26,10 @@
             <section class="row m-auto justify-content-center">
                 <div class="col-md-5">
                     <?php
+                        $campusCookie = $_COOKIE['campus-cookie'];
+                        unserialize($campusCookie);
+                        $campusSigla = $campusCookie['sigla'];
+
                         echo '<h3 class="text-center">' . $campusSigla . '</h3>';
                         echo '<h5 class="text-center text-muted">' . $campusName . '</h5>';
                         echo '<p class="text-center text-muted">' . $campusAddress . '</p>';
